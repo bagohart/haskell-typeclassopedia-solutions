5352d95967991b7c206f272476ca16c362bd58b8
bagohart <bagohart@gmx.de>
2020-11-30 10:48:08 +0100

    added remaining exercise solutions for arrow_apply

--- arrow_apply.hs
+++ arrow_apply.hs
@@ -8,9 +8,6 @@ import Data.Monoid
 -- class Arrow arr -> ArrowApply arr where 
 --  app :: (b `arr` c, b) `arr` c
 --  ^ this looks weird. apparently, the arrow in the first argument is applied to the value in the first argument. hm.
---  todo:
---  app2
---  various instances to show equivalence between ArrowApply and Monad
 
 -- instance for (->)
 newtype Func a b = Func { runFunc :: a -> b }
@@ -36,3 +33,51 @@ app2' = Func $ \b -> Func $ \(Func g) -> g b
 -- exercise: now use app, but not specific properties of Func to implement this.
 app2'' :: ArrowApply ar => ar b (ar (ar b c) c)
 app2'' = arr $ \b -> ((arr $ \g -> (g,b)) >>> app)
+
+-- exercise: Kleisli is an ArrowApply
+newtype Kleisli' m a b = Kleisli' { runKleisli' :: a -> m b }
+
+instance Monad m => Category (Kleisli' m) where 
+    id :: Kleisli' m a a
+    id = Kleisli' return 
+    (.) :: Kleisli' m b c -> Kleisli' m a b -> Kleisli' m a c
+    (.) (Kleisli' g) (Kleisli' h) = Kleisli' (h >=> g)
+    -- (.) (Kleisli' g) (Kleisli' h) = Kleisli' (\a -> h a >>= g)
+    -- (.) (Kleisli' g) (Kleisli' h) = Kleisli' (\a -> h a >>= \b -> g b)
+    
+instance Monad m => Arrow (Kleisli' m) where 
+    arr :: (b -> c) -> Kleisli' m b c
+    arr g = Kleisli' $ return Prelude.. g
+
+    first :: Kleisli' m b c -> Kleisli' m (b,d) (c,d)
+    first (Kleisli' g) = Kleisli' $ \ ~(b,d) -> do 
+                                            c <- g b
+                                            return (c,d)
+
+instance Monad m => ArrowApply (Kleisli' m) where 
+    app :: Kleisli' m (Kleisli' m b c, b) c
+    app = Kleisli' $ \(Kleisli' g,b) -> g b
+    -- app = Kleisli' $ \x -> runKleisli' (fst x) $ snd x
+
+newtype ArrowMonad' a b = ArrowMonad' (a () b)
+
+instance ArrowApply a => Functor (ArrowMonad' a) where 
+    fmap :: (b -> c) -> ArrowMonad' a b -> ArrowMonad' a c
+    fmap g (ArrowMonad' a) = ArrowMonad' $ a >>> (arr g)
+
+instance ArrowApply a => Applicative (ArrowMonad' a) where 
+    pure :: b -> ArrowMonad' a b
+    pure x = ArrowMonad' $ arr $ \() -> x
+
+    (<*>) :: ArrowMonad' a (b -> c) -> ArrowMonad' a b -> ArrowMonad' a c
+    (<*>) (ArrowMonad' g) (ArrowMonad' b) = ArrowMonad' $ (g &&& b) >>> (arr $ \(g,b) -> g b)
+
+instance ArrowApply a => Monad (ArrowMonad' a) where 
+    return :: b -> ArrowMonad' a b
+    return = pure
+
+    (>>=) :: ArrowMonad' a b -> (b -> ArrowMonad' a c) -> ArrowMonad' a c
+    (>>=) (ArrowMonad' b) g = ArrowMonad' $ b >>> ((arr (runArrowMonad' Prelude.. g)) &&& (arr (const ()))) >>> app
+
+runArrowMonad' :: ArrowMonad' a b -> a () b
+runArrowMonad' (ArrowMonad' a) = a
